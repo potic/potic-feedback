62cf1dc43264a460b78a8904a2ae4246f40b4a7c
yermilov <yermilov17@gmail.com>
2018-03-09 01:37:03 +0200

    expose port

--- src/main/scripts/deploy.sh
+++ src/main/scripts/deploy.sh
@@ -21,7 +21,7 @@ warn "Pulling latest docker image..."
 docker pull potic/potic-feedback:$TAG_TO_DEPLOY
 
 warn "Starting docker image..."
-docker run -dit --name potic-feedback --restart on-failure --link potic-articles -e LOG_PATH=/mnt/logs -v /mnt/logs:/mnt/logs -e LOGZIO_TOKEN=$LOGZIO_TOKEN potic/potic-feedback:$TAG_TO_DEPLOY
+docker run -dit --name potic-feedback --restart on-failure --link potic-articles -e LOG_PATH=/mnt/logs -v /mnt/logs:/mnt/logs -e LOGZIO_TOKEN=$LOGZIO_TOKEN -p 40411:8080 potic/potic-feedback:$TAG_TO_DEPLOY
 
 warn "Currently running docker images"
 docker ps -a
